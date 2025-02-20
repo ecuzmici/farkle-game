794f97aab364a1351be363b31bdb3a27b4b23c15
Emil <ecuzmici@gmail.com>
2025-02-20 14:50:01 +0100

    small env fix

--- src/app/api/livekit/token/route.ts
+++ src/app/api/livekit/token/route.ts
@@ -5,8 +5,8 @@ export async function POST(req: Request) {
   const { roomName, participantName } = await req.json();
   
   const at = new AccessToken(
-    process.env.LIVEKIT_API_KEY!,
-    process.env.LIVEKIT_API_SECRET!,
+    process.env.NEXT_PUBLIC_LIVEKIT_API_KEY!,
+    process.env.NEXT_PUBLIC_LIVEKIT_API_SECRET!,
     {
       identity: participantName,
       name: participantName,
